5dc59fc84590511dffc5ec516d2fbe66e4575a28
Anurag Upadhyay <anuragle100gmail.com>
2025-09-18 20:21:22 +0530

    Add database schema and functions for user management and leave policies
    - Created tables for classes, departments, sections, leave policies, roles, users, user profiles, access controls, leave status, user leaves, class teachers, notice status, notices, user refresh tokens, permissions, notice recipient types, and user leave policy.
    - Implemented functions for adding/updating staff and students, including validation and error handling.
    - Developed a function to retrieve dashboard data for users, including student, teacher, and parent statistics, notices, leave policies, leave history, celebrations, and upcoming leaves.
    - Added a function to fetch notices based on user roles and access rights.

---
+++ backend/src/middlewares/authenticate-token.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+const { ApiError } = require("../utils");
+const { env } = require("../config");
+
+const authenticateToken = (req, res, next) => {
+  console.log("Authenticating token...");
+  const accessToken = req.cookies.accessToken;
+  const refreshToken = req.cookies.refreshToken;
+
+  console.log("Access Token:", accessToken);
+  console.log("Refresh Token:", refreshToken);
+
+  if (!accessToken || !refreshToken) {
+    throw new ApiError(401, "Unauthorized. Please provide valid tokens.");
+  }
+
+  jwt.verify(accessToken, env.JWT_ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      throw new ApiError(
+        401,
+        "Unauthorized. Please provide valid access token."
+      );
+    }
+
+    jwt.verify(
+      refreshToken,
+      env.JWT_REFRESH_TOKEN_SECRET,
+      (err, refreshToken) => {
+        if (err) {
+          throw new ApiError(
+            401,
+            "Unauthorized. Please provide valid refresh token."
+          );
+        }
+
+        req.user = user;
+        req.refreshToken = refreshToken;
+        next();
+      }
+    );
+  });
+};
+
+module.exports = { authenticateToken };
---
+++ seed_db/seed-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO classes (id, name, sections) VALUES (1, '10', 'A');
+INSERT INTO classes (id, name, sections) VALUES (5, '11', 'B');
+INSERT INTO classes (id, name, sections) VALUES (9, '12', 'C');
+
+
+INSERT INTO sections (id, name) VALUES (1, 'A');
+INSERT INTO sections (id, name) VALUES (2, 'B');
+INSERT INTO sections (id, name) VALUES (3, 'C');
